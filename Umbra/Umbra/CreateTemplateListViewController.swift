c20490e5ae7b24a6466e6eb29cdd4a06333a794d
Kyle Swygert <kyleswygert@gmail.com>
2020-04-14 15:25:07 -0700

    Revert "working on creating new template list while adding new rows to tableview when a button is clicked."
    This reverts commit 4c2d3d10a79d56955032f77f818f4108d9fdac92.

--- Umbra/Umbra/CreateTemplateListViewController.swift
+++ Umbra/Umbra/CreateTemplateListViewController.swift
@@ -9,19 +9,13 @@
 import UIKit
 
 class CreateTemplateListViewController: UITableViewController {
-    
-    @IBOutlet weak var listTitleTextField: UITextField!
-    
-    @IBOutlet weak var listDescriptionTextField: UITextField!
-    
-    
 
-    var newListItems: [ListItem] = []
+    
     
     override func viewWillAppear(_ animated: Bool) {
         // stuff
         
-        // deactivate the tabBar from this view.
+        // deactivate the navigation bar from this view.
         
         
     }
@@ -34,75 +28,20 @@ class CreateTemplateListViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        
-        
-        
-        
-        
-        tableView.rowHeight = 55
-        
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 3
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        
-        // TODO: how to change the number of rows to grow with more listItems being added to the list???
-        
-        
-        var numRows = 0
-        
-        switch section {
-        case 0: // list title
-            numRows = 1
-            break
-            
-        case 1: // list description
-            numRows = 1
-            break
-        
-        case 2: // list items
-            numRows = newListItems.count + 2 // add 1 for the cell with the button to add another row to the table
-            break
-        default:
-            numRows = 1
-        }
-        
-        
-        return numRows
-        
-        //return newListItems.count
-        
-        //return 3
+        return 0
     }
 
-    
-    @IBAction func saveButtonTapped(_ sender: UIButton) {
-        
-        print("save button tapped")
-        
-        // create a TemplateList object from the data entered in the view, then pass the object in a segue from this view to the TemplateTableViewController.
-        
-    
-        
-        // print the title and label
-        
-        print("List Title:          \(listTitleTextField.text!)")
-        print("List Description:    \(listDescriptionTextField.text!)")
-        
-        
-        
-        
-    }
-    
-    
-    
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
@@ -148,24 +87,14 @@ class CreateTemplateListViewController: UITableViewController {
     }
     */
 
-    
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-        
-        
-        // create a TemplateList object from the data in this view, then pass that new object back to the TemplateTableViewController to then add the new TemplateList to the app DataModel.
-        
-        
-        
-        // segue back to the TemplatesTableViewController. 
-        
-        
-        
     }
-    
+    */
 
 }
