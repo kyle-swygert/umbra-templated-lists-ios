500314c4fbd4a9c9acabf47d83bc8f60f845db3d
Kyle Swygert <kyleswygert@gmail.com>
2020-04-27 11:54:43 -0700

    usableList instantiation picker view has been implemented, now working on creating an instantiated list object with checkboxes.

---
+++ Umbra/Umbra/InstantiateUsableListViewController.swift
@@ -0,0 +1,253 @@
+//
+//  InstantiateUsableListViewController.swift
+//  Umbra
+//
+//  Created by Kyle Swygert on 4/25/20.
+//  Copyright © 2020 Kyle Swygert. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+class InstantiateUsableListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    
+    
+    var managedObjectContext: NSManagedObjectContext!
+    var appDelegate: AppDelegate!
+    
+    
+    
+    
+    
+    // this picker will be populated with the names of the listTemplates that are stored in the app.
+    @IBOutlet weak var templateListPicker: UIPickerView!
+    
+    
+    // this table view will display all the information that is associated with the currently selected list that is in the picker above. display the name, description, and the list items.
+    @IBOutlet weak var currTemplateTableView: UITableView!
+    
+    
+    var allTemplates: [ListTemplate] = []
+    
+    
+    var currSelectedTemplate: ListTemplate!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        
+        appDelegate = UIApplication.shared.delegate as? AppDelegate
+        managedObjectContext = appDelegate.persistentContainer.viewContext
+        
+        // load all the templateLists into the program here.
+        // can use the same code as the TemplateTableViewController.
+        
+        
+        // populate the picker with the names of the templates in allTemplates.
+        
+        self.templateListPicker.delegate = self
+        self.templateListPicker.dataSource = self
+        
+        self.currTemplateTableView.delegate = self
+        self.currTemplateTableView.dataSource = self
+        
+        
+        // set the currently selected ListTemplate
+        // NOTE: I think that my app will only work if there is at least ione template in the app. try to see if that is the case, and if it is, attempt to fix it by not allowing the action to occur and popping up an alert saying "there are no templates to chose from.
+        // default. select the first item to be the selected template.
+        currSelectedTemplate = allTemplates[0]
+        
+        
+    }
+    
+    // MARK: - Picker Functions.
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        // the picker will have the same number of rows as the number of templates in allTemplates array.
+        allTemplates.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        // return the name of the template for that row of the picker.
+        return allTemplates[row].templateName
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        // the row for the picker was selected, display the contents of the description and the listItems in the tableView.
+        
+        print("didSelectRow()...")
+        
+        let count = currSelectedTemplate.listItems.count + 1
+        
+        currSelectedTemplate = nil
+        //currTemplateTableView.reloadData()
+        
+        
+        // set the new selected picker item then populate the table.
+        
+        currTemplateTableView.beginUpdates()
+        
+        
+        // insert the rows into the table here.
+        currTemplateTableView.deleteRows(at: (0..<count).map({ (i) in IndexPath(row: i, section: 0)}), with: .fade)
+        
+        
+        currSelectedTemplate = allTemplates[row]
+        
+        let newCount = currSelectedTemplate.listItems.count + 1
+        
+        currTemplateTableView.insertRows(at: (0..<newCount).map({ (i) in IndexPath(row: i, section: 0)}), with: .fade)
+        
+        
+        currTemplateTableView.endUpdates()
+        
+        
+        
+        
+        currTemplateTableView.reloadData()
+        
+        
+        
+        
+//
+//
+//        var currTemplateIndex = templateListPicker.selectedRow(inComponent: 0)
+//
+//        var currListTemplate = allTemplates[currTemplateIndex]
+//
+//        print(currListTemplate.templateName)
+//
+//
+//        // clear the tableView contents.
+//        //self.currTemplateTableView
+//
+//
+//        currTemplateTableView.beginUpdates()
+//
+//        currSelectedTemplate = allTemplates[currTemplateIndex]
+//
+//        //currTemplateTableView.insertRows(at: <#T##[IndexPath]#>, with: <#T##UITableView.RowAnimation#>)
+//
+//        var row = 0
+//
+//
+//        // loop to insert the rows into the tableView.
+//        for item in currSelectedTemplate.listItems {
+//
+//            var currIndex = IndexPath(row: row, section: 0)
+//
+//            currTemplateTableView.insertRows(at: [currIndex], with: .fade)
+//
+//
+//            row += 1
+//        }
+//
+//
+//
+//        // remove all the previous rows.
+//
+//        // add the rows for the currently selected ListTemplate.
+//
+//
+//
+//        currTemplateTableView.endUpdates()
+//
+//
+//        currTemplateTableView.reloadData()
+//
+//        // display all the currentlye selected ListTemplate items.
+//
+//
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func acceptButtonTapped(_ sender: UIButton) {
+        
+        // the template has been selected, send this information back to the previous view to create a new instance of the list.
+        
+        
+        //var selectedTemp = self.templateListPicker.selectedRow(inComponent: 0)
+        
+        
+        
+        // trying to set the selected currSelectedTemplate object to the row that is selected in the picker in the view.
+        //self.currSelectedTemplate = templateListPicker.row
+        
+        
+    }
+    
+    // MARK: - TableView Functions
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // the currently selected template object will change depending on the item selected in the picker.
+        // when the picker selection is changed, the table view will be changed to show all the items that are part of that list.
+        
+        
+        // need to return +1 to account for the description of the list, as well as the listItems themselves.
+        return currSelectedTemplate.listItems.count + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // use the DisplayTemplateCell object to show all the items in the tableView.
+        
+        let cell = currTemplateTableView.dequeueReusableCell(withIdentifier: "displayTemplateCell") as! DisplayTemplateCell
+        
+        // NOTE: For testing
+        cell.displayCellLabel.text = "testing display string"
+        
+        // TODO: Display the actual content of the cell just like in the DisplayTemplateTableViewController
+        
+        // NOTE: Only display the description and the ListItems, since the name / title of the template will be in the picker view.
+        
+        
+        if indexPath.row == 0 {
+            // display the description of the list
+            cell.displayCellLabel.text = currSelectedTemplate.description
+            
+            
+        } else {
+            // display that current item in the list.
+            let listInd = indexPath.row - 1
+            
+            cell.displayCellLabel.text = currSelectedTemplate.listItems[listInd].itemText
+            
+        }
+        
+        return cell
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Umbra/Umbra/ListsTableViewController.swift
+++ Umbra/Umbra/ListsTableViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import CoreData
 
 class ListsTableViewController: UITableViewController {
     
+    
+    
+    var managedObjectContext: NSManagedObjectContext!
+    var appDelegate: AppDelegate!
+    
+    var currUsableListArr: [UsableList] = []
     
     @IBAction func newListButtonTapped(_ sender: UIButton) {
         
@@ -35,6 +42,17 @@ class ListsTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        
+        
+               appDelegate = UIApplication.shared.delegate as? AppDelegate
+               managedObjectContext = appDelegate.persistentContainer.viewContext
+               
+               
+        
+        
+        
+        
     }
 
     // MARK: - Table view data source
@@ -94,14 +112,185 @@ class ListsTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
+    
+    func templateData2TemplateObj(_ dataObj: NSManagedObject) -> ListTemplate {
+        
+        let tempName = (dataObj.value(forKey: "templateName") as? String)!
+        let tempID = (dataObj.value(forKey: "templateID") as? String)!
+        let tempDesc = (dataObj.value(forKey: "templateDescription") as? String)!
+        
+        
+        return ListTemplate(templateName: tempName, description: tempDesc, listItems: [], templateID: tempID)
+        
+    }
+    
+    
+    // this function will create ListItems that are to be used in the Template View, so the boolean values of the isChecked members will always be set to false. This will be different when a usable list is being created though.
+    func listItemData2ListItemObj(_ dataObj: NSManagedObject) -> ListItem {
+        
+        let itemID = (dataObj.value(forKey: "itemID") as? String)!
+        let itemText = (dataObj.value(forKey: "itemText") as? String)!
+        
+        return ListItem(itemText: itemText, itemID: itemID)
+        
+    }
+    
+    func printTemplateDataObj(_ template: NSManagedObject) {
+        
+        let name = template.value(forKey: "templateName") as? String
+        
+        print("Current Template Name: \(name!)")
+        
+        
+    }
+    
+    
+    // this function will retrieve all the ListTemplate objects from the DataModel. ListItems have not been retrieved yet.
+    func getTemplateItems() -> [NSManagedObject] {
+        
+        var allTemplates: [NSManagedObject] = []
+        
+        let fetchReq = NSFetchRequest<NSManagedObject>(entityName: "TemplateListData")
+        
+        do {
+            allTemplates = try self.managedObjectContext.fetch(fetchReq)
+        } catch {
+            print("getTemplateItems() error: \(error)")
+        }
+        
+        
+        return allTemplates
+    }
+    
+    
+    // this function will retrieve all the ListItems associated with the parent object that has the parentID
+    func getListItemsForParentTemplate(_ parentTemplate: NSManagedObject) -> [NSManagedObject] {
+        
+        let parentID = parentTemplate.value(forKey: "templateID") as? String
+        
+        print("getListItemsForParentTemplate()")
+        print("Current parentID: \(parentID!)")
+        
+        var allListItems: [NSManagedObject] = []
+        
+        // fetch the items that have the same parentID as the input parameter
+        let fetchReq = NSFetchRequest<NSManagedObject>(entityName: "ListItemData")
+        fetchReq.predicate = NSPredicate(format: "parentID == %@", parentID!)
+        
+        do {
+            allListItems = try self.managedObjectContext.fetch(fetchReq)
+        } catch {
+            print("getListItemsForParentTemplate() error: \(error)")
+        }
+        
+        
+        return allListItems
+        
+        
+        
+        
+    }
+    
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        if (segue.identifier == "fromListsToInstantiateListView") {
+            // the app will now be transferring to the view to instantiate a UsableList.
+            
+            // load all the ListTemplates into memory from the DataModel, then seld that data to the destination view.
+            
+            var templateListsArr: [ListTemplate] = []
+            
+            
+            // TODO: retrieve the templateLists from the DataModel and display them to the screen here.
+            var allTemplates = getTemplateItems()
+            
+            // for each item in the allTemplates list, get the ListItems for the specified parentID
+            
+            for temp in allTemplates {
+                
+                printTemplateDataObj(temp)
+                
+                var currList = templateData2TemplateObj(temp)
+                
+                let childrenItems = getListItemsForParentTemplate(temp)
+                
+                
+                print(childrenItems)
+                
+                // NOTE: currently temp holds the ListTemplate object data, and the childrenItems array holds all the ListItem objects in it.
+                // instantiate actual objects from these pieces of data, then add the whole ListTemplate to the templateListArr to display them on the app!
+                
+                for child in childrenItems {
+                    // create a ListItem object from child and add it to the newly created ListTemplate object.
+                    
+                    var currChild = listItemData2ListItemObj(child)
+                    
+                    currList.listItems.append(currChild)
+                    
+                    
+                }
+                
+                // append the newly created list from the DataModel onto the array to populate the tableView.
+                templateListsArr.append(currList)
+                
+            }
+            
+            
+            
+            // prepare for the segue by setting the destination view's data to the data that was just retrieved from the DataModel.
+            
+            let destVC = segue.destination as! InstantiateUsableListViewController
+            
+            // the loaded lists are passed to the view before the class is fully loaded into the app. 
+            destVC.allTemplates = templateListsArr
+            
+            
+            
+            
+           
+        } // end of block for first segue block.
+        
+        
+        
+        
+        
+    } // end of prepare()
+    
+    
+    
+    @IBAction func unwindFromInstantiateListView(sender: UIStoryboardSegue) {
+        
+        print("unwindFromInstantiateListView()")
+        
+        let prevVC = sender.source as! InstantiateUsableListViewController
+        
+        // get the selected list from the previous view and add a new instance of that UsableList to the tableView in this controller.
+        
+        print("selected ListTemplate: \(prevVC.currSelectedTemplate.templateName)")
+        
+        
+        // now instantiate a version of this template that can be used by the user with checkBoxes and add that object to the tableView list of items.
+        
+        // append the new UsableList to the currUsableListArr array
+        
+        
+        
+        // reload the tableView data after appending to the array. 
+        
+        
+        
+        
     }
-    */
+    
+    
+    
+    
 
-}
+} // end of class
